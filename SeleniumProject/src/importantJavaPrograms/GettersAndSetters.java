75196cb417d5f1e0108087b6c8fbb5cbea29fad4
akshay <127499873+Akshayholkar123@users.noreply.github.com>
2023-04-25 12:19:14 +0530

    Update GettersAndSetters.java

--- SeleniumProject/src/importantJavaPrograms/GettersAndSetters.java
+++ SeleniumProject/src/importantJavaPrograms/GettersAndSetters.java
@@ -3,7 +3,7 @@ package importantJavaPrograms;
 public class GettersAndSetters {
 private String s= "Hello World";
 	public static void main(String[] args) {
-		
+		System.out.println("this program generate getters nd setters ")
 
 	}
 	public String getS() {
