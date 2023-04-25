2b16cb13deac437983b1ad9688471a8e7d4ec8ff
akshay  holkar <akshay  holkar@LAPTOP-O03F5P6F>
2023-04-25 12:10:38 +0530

    getters and setters

---
+++ SeleniumProject/src/importantJavaPrograms/GettersAndSetters.java
@@ -0,0 +1,16 @@
+package importantJavaPrograms;
+
+public class GettersAndSetters {
+private String s= "Hello World";
+	public static void main(String[] args) {
+		
+
+	}
+	public String getS() {
+		return s;
+	}
+	public void setS(String s) {
+		this.s = s;
+	}
+
+}
